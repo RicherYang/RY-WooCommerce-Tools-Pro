a0a088d4f362ba9ef15463f541c622cfd3c6f79f
RicherYang <fantasyworldidvtw@gmail.com>
2020-03-15 20:14:46 +0800

    update 1.1.0.1

--- class.ry-wt-p.update.php
+++ class.ry-wt-p.update.php
@@ -12,8 +12,8 @@ final class RY_WTP_update {
 			return;
 		}
 
-		if( version_compare($now_version, '1.1.0', '<' ) ) {
-			RY_WTP::update_option('version', '1.1.0');
+		if( version_compare($now_version, '1.1.0.1', '<' ) ) {
+			RY_WTP::update_option('version', '1.1.0.1');
 		}
 	}
 }
--- readme.txt
+++ readme.txt
@@ -4,4 +4,4 @@ Tags: woocommerce, payment, gateway, ecpay
 Requires at least: 5.0
 Requires PHP: 5.6.20
 Tested up to: 5.3.2
-Stable tag: 1.1.0
+Stable tag: 1.1.0.1
