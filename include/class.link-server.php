755cace4109b89341bba886f198964cad41d9e96
RicherYang <fantasyworldidvtw@gmail.com>
2020-05-25 13:21:07 +0800

    update 1.1.7

--- include/class.link-server.php
+++ include/class.link-server.php
@@ -3,66 +3,63 @@ defined('RY_WTP_VERSION') or exit('No direct script access allowed');
 
 final class RY_WTP_link_server
 {
-    private static $api_url = 'https://store.richer.tw/wp-admin/admin-ajax.php';
+    private static $api_url = 'https://store.richer.tw/wp-json/ry/';
     private static $plugin_type = 'ry-woocommerce-tools-pro';
 
     public static function check_version()
     {
-        $response = wp_remote_post(self::$api_url, [
-            'timeout' => 10,
-            'body' => [
-                'action' => 'check_version',
-                'type' => self::$plugin_type
-            ]
-        ]);
+        $response = wp_remote_get(self::$api_url . 'products/' . self::$plugin_type);
 
-        if (!is_wp_error($response)) {
-            if ($response['response']['code'] == '200' && isset($response['body'])) {
-                return json_decode($response['body'], true);
-            }
-        }
-
-        return false;
+        return self::decode_response($response);
     }
 
     public static function activate_key()
     {
-        $response = wp_remote_post(self::$api_url, [
-            'timeout' => 10,
-            'body' => [
-                'action' => 'activate',
+        $response = wp_remote_post(self::$api_url . 'license/activate/' . self::$plugin_type, [
+            'user-agent' => self::get_user_agent(),
+            'headers' => [
+                'Content-Type' => 'application/json;charset=' . get_bloginfo('charset'),
+            ],
+            'body' => wp_json_encode([
                 'license_key' => RY_WTP::get_option('pro_Key'),
-                'domain' => get_site_url(),
-                'type' => self::$plugin_type
-            ]
+                'domain' => get_site_url()
+            ])
         ]);
 
-        if (!is_wp_error($response)) {
-            if ($response['response']['code'] == '200' && isset($response['body'])) {
-                return json_decode($response['body'], true);
-            }
-        }
-
-        return false;
+        return self::decode_response($response);
     }
 
     public static function expire_data()
     {
-        $response = wp_remote_post(self::$api_url, [
-            'timeout' => 10,
-            'body' => [
-                'action' => 'expire_info',
-                'domain' => get_site_url(),
-                'type' => self::$plugin_type
-            ]
+        $response = wp_remote_post(self::$api_url . 'license/expire/' . self::$plugin_type, [
+            'user-agent' => self::get_user_agent(),
+            'headers' => [
+                'Content-Type' => 'application/json;charset=' . get_bloginfo('charset'),
+            ],
+            'body' => wp_json_encode([
+                'domain' => get_site_url()
+            ])
         ]);
 
-        if (!is_wp_error($response)) {
-            if ($response['response']['code'] == '200' && isset($response['body'])) {
-                return json_decode($response['body'], true);
-            }
+        return self::decode_response($response);
+    }
+
+    protected static function decode_response($response)
+    {
+        if (is_wp_error($response)) {
+            return false;
         }
 
-        return false;
+        if (200 != wp_remote_retrieve_response_code($response)) {
+            return false;
+        }
+
+        $body = wp_remote_retrieve_body($response);
+        return @json_decode($body, true);
+    }
+
+    protected static function get_user_agent()
+    {
+        return sprintf('RY_WTP %s (WordPress/%s WooCommerce/%s)', RY_WTP_VERSION, get_bloginfo('version'), WC_VERSION);
     }
 }
--- include/class.updater.php
+++ include/class.updater.php
@@ -1,49 +1,53 @@
 <?php
-defined('RY_WTP_VERSION') OR exit('No direct script access allowed');
+defined('RY_WTP_VERSION') or exit('No direct script access allowed');
 
-final class RY_WTP_updater {
-	private static $initiated = false;
-	
-	public static function init() {
-		if( !self::$initiated ) {
-			self::$initiated = true;
+final class RY_WTP_updater
+{
+    private static $initiated = false;
 
-			add_filter('pre_set_site_transient_update_plugins', [__CLASS__, 'transient_update_plugins']);
-		}
-	}
+    public static function init()
+    {
+        if (!self::$initiated) {
+            self::$initiated = true;
 
-	public static function check_update() {
-		$update_plugins = get_site_transient('update_plugins');
-		set_site_transient('update_plugins', $update_plugins);
-	}
+            add_filter('pre_set_site_transient_update_plugins', [__CLASS__, 'transient_update_plugins']);
+        }
+    }
 
-	public static function transient_update_plugins($transient) {
-		$json = RY_WTP_link_server::check_version();
+    public static function check_update()
+    {
+        $update_plugins = get_site_transient('update_plugins');
+        set_site_transient('update_plugins', $update_plugins);
+    }
 
-		if( is_array($json) && isset($json['version']) ) {
-			set_site_transient(RY_WTP::$option_prefix . 'checktime', time());
+    public static function transient_update_plugins($transient)
+    {
+        $json = RY_WTP_link_server::check_version();
 
-			$item = (object) array(
-				'slug' => 'ry-woocommerce-tools-pro',
-				'plugin' => RY_WTP_PLUGIN_BASENAME,
-				'new_version'=> $json['version'],
-				'package'=> $json['url']
-			);
-			if( version_compare(RY_WTP_VERSION, $json['version'], '<') ) {
-				if( empty($transient) ) {
-					$transient = new stdClass;
-				}
-				$transient->last_checked = time();
-				$transient->response[RY_WTP_PLUGIN_BASENAME] = (object) $item;
-			} else {
-				if( isset($transient->response) ) {
-					unset($transient->response[RY_WTP_PLUGIN_BASENAME]);
-				}
-			}
-		}
+        if (is_array($json) && isset($json['version'])) {
+            set_site_transient(RY_WTP::$option_prefix . 'checktime', time());
 
-		return $transient;
-	}
+            $item = (object) array(
+                'slug' => 'ry-woocommerce-tools-pro',
+                'plugin' => RY_WTP_PLUGIN_BASENAME,
+                'new_version'=> $json['version'],
+                'package'=> $json['url']
+            );
+            if (version_compare(RY_WTP_VERSION, $json['version'], '<')) {
+                if (empty($transient)) {
+                    $transient = new stdClass;
+                }
+                $transient->last_checked = time();
+                $transient->response[RY_WTP_PLUGIN_BASENAME] = (object) $item;
+            } else {
+                if (isset($transient->response)) {
+                    unset($transient->response[RY_WTP_PLUGIN_BASENAME]);
+                }
+            }
+        }
+
+        return $transient;
+    }
 }
 
 RY_WTP_updater::init();
