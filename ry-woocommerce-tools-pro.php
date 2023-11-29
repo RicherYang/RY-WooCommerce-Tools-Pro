d5feedf09ef59ff52758503fdb15a4585d40975c
Richer Yang <fantasyworldidvtw@gmail.com>
2023-11-29 12:17:45 +0800

    update 3.0.2

--- ry-woocommerce-tools-pro.php
+++ ry-woocommerce-tools-pro.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: RY WooCommerce Tools Pro
- * Plugin URI: https://richer.tw/ry-woocommerce-tools-pro/
+ * Plugin URI: https://ry-plugin.com/ry-woocommerce-tools-pro/
  * Description: WooCommerce paymet and shipping tools
- * Version: 3.0.1
- * Requires at least: 6.0
+ * Version: 3.0.2
+ * Requires at least: 6.2
  * Requires PHP: 7.4
  * Author: Richer Yang
  * Author URI: https://richer.tw/
@@ -14,12 +14,12 @@
  * Text Domain: ry-woocommerce-tools-pro
  * Domain Path: /languages
  *
- * WC requires at least: 7
+ * WC requires at least: 8
  */
 
 function_exists('plugin_dir_url') or exit('No direct script access allowed');
 
-define('RY_WTP_VERSION', '3.0.1');
+define('RY_WTP_VERSION', '3.0.2');
 define('RY_WTP_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('RY_WTP_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('RY_WTP_PLUGIN_BASENAME', plugin_basename(__FILE__));
