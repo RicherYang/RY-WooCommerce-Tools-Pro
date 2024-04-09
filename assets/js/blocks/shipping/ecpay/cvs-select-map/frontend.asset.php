069ae3f870465db6773ceb58289feefbe824b039
Richer Yang <fantasyworldidvtw@gmail.com>
2024-04-09 21:21:33 +0800

    配合 WP 6.5 新增 PHP 語系檔

--- assets/js/blocks/shipping/ecpay/cvs-select-map/frontend.asset.php
+++ assets/js/blocks/shipping/ecpay/cvs-select-map/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'a78a0be94b2f65cfbea8');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'eda5f5c011da76e20662');
