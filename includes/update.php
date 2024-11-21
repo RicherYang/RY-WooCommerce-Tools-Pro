8edcc47cc4be3d6fb44444e1a651b1ab1472403e
Richer Yang <fantasyworldidvtw@gmail.com>
2024-11-21 14:17:30 +0800

    調整綠界物流狀態表

--- includes/update.php
+++ includes/update.php
@@ -16,8 +16,8 @@ final class RY_WTP_Update
             RY_WTP::update_option('version', '3.0.0');
         }
 
-        if (version_compare($now_version, '3.4.10', '<')) {
-            RY_WTP::update_option('version', '3.4.10', true);
+        if (version_compare($now_version, '3.4.11', '<')) {
+            RY_WTP::update_option('version', '3.4.11', true);
         }
     }
 }
--- templates/order/order-ecpay-shipping-info.php
+++ templates/order/order-ecpay-shipping-info.php
@@ -44,7 +44,7 @@ if (count($shipping_info_list)) { ?>
                     echo esc_html_x('Transporting', 'Shipping status', 'ry-woocommerce-tools-pro');
                 } elseif (in_array($shipping_info['status'], apply_filters('ry_ecpay_shipping_status_info_wait_pick', [2063, 2073, 3018]))) {
                     echo esc_html_x('Waiting for pick up', 'Shipping status', 'ry-woocommerce-tools-pro');
-                } elseif (in_array($shipping_info['status'], apply_filters('ry_ecpay_shipping_status_info_completed', [2067, 3003, 3022]))) {
+                } elseif (in_array($shipping_info['status'], apply_filters('ry_ecpay_shipping_status_info_completed', [2067, 3003, 3022, 3308, 3309]))) {
                     echo esc_html_x('Completed', 'Shipping status', 'ry-woocommerce-tools-pro');
                 } elseif (in_array($shipping_info['status'], apply_filters('ry_ecpay_shipping_status_info_overdue', [2065, 2070, 2072, 2074, 2076, 3019, 3020, 3023, 3025]))) {
                     echo esc_html_x('Overdue return', 'Shipping status', 'ry-woocommerce-tools-pro');
