87c6891a110927b99e580a80ca659c2438a0d29b
Richer Yang <fantasyworldidvtw@gmail.com>
2025-05-27 21:08:49 +0800

    修正更新頁面與其他外掛可能的不相容問題

--- woocommerce/admin/settings/html/version-info.php
+++ woocommerce/admin/settings/html/version-info.php
@@ -5,8 +5,8 @@
     <td class="forminp">
         <?php esc_html_e('Now Version:', 'ry-woocommerce-tools-pro'); ?> <?php echo esc_html($version); ?>
         <?php if ($version_info && version_compare($version, $version_info['version'], '<')) { ?>
-        <?php set_site_transient('update_plugins', []); ?>
-        <br><span style="color:blue"><?php esc_html_e('New Version:', 'ry-woocommerce-tools-pro'); ?></span> <?php echo esc_html($version_info['version']); ?>
+        <br>
+        <span style="color:blue"><?php esc_html_e('New Version:', 'ry-woocommerce-tools-pro'); ?></span> <?php echo esc_html($version_info['version']); ?>
         <a href="<?php echo esc_url(wp_nonce_url(self_admin_url('update.php?action=upgrade-plugin&plugin=' . RY_WTP_PLUGIN_BASENAME), 'upgrade-plugin_' . RY_WTP_PLUGIN_BASENAME)); ?>">
             <?php esc_html_e('update plugin', 'ry-woocommerce-tools-pro'); ?>
         </a>
