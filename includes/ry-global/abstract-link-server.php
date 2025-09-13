26a31f7b31643ce8d28dffd6ab053d639a8e52df
Richer Yang <fantasyworldidvtw@gmail.com>
2025-09-13 17:27:29 +0800

    修正不同外掛共用程式重複載入問題

--- includes/ry-global/abstract-link-server.php
+++ includes/ry-global/abstract-link-server.php
@@ -1,97 +1,95 @@
 <?php
 
-if (class_exists('RY_Abstract_Link_Server')) {
-    return;
-}
-
-abstract class RY_Abstract_Link_Server
-{
-    protected $api_url = 'https://ry-plugin.com/wp-json/ry/v2/';
-
-    protected $plugin_slug;
-
-    abstract protected function get_user_agent();
-
-    public function check_version(): bool|array
+if (!class_exists('RY_Abstract_Link_Server', false)) {
+    abstract class RY_Abstract_Link_Server
     {
-        @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
+        protected $api_url = 'https://ry-plugin.com/wp-json/ry/v2/';
 
-        $response = wp_remote_get($this->api_url . 'products/' . $this->plugin_slug, [
-            'timeout' => 20,
-            'httpversion' => '1.1',
-            'user-agent' => $this->get_user_agent(),
-        ]);
+        protected $plugin_slug;
 
-        return $this->decode_response($response);
-    }
+        abstract protected function get_user_agent();
 
-    public function get_info(): bool|array
-    {
-        @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
+        public function check_version(): bool|array
+        {
+            @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
 
-        $response = wp_remote_get($this->api_url . 'products/info/' . $this->plugin_slug, [
-            'timeout' => 20,
-            'httpversion' => '1.1',
-            'user-agent' => $this->get_user_agent(),
-        ]);
+            $response = wp_remote_get($this->api_url . 'products/' . $this->plugin_slug, [
+                'timeout' => 20,
+                'httpversion' => '1.1',
+                'user-agent' => $this->get_user_agent(),
+            ]);
 
-        return $this->decode_response($response);
-    }
-
-    public function expire_data(): bool|array
-    {
-        @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
-
-        $response = wp_remote_post($this->api_url . 'license/expire/' . $this->plugin_slug, [
-            'timeout' => 20,
-            'httpversion' => '1.1',
-            'user-agent' => $this->get_user_agent(),
-            'headers' => [
-                'Content-Type' => 'application/json;charset=' . get_bloginfo('charset'),
-            ],
-            'body' => wp_json_encode([
-                'domain' => get_option('siteurl'),
-            ]),
-        ]);
-
-        return $this->decode_response($response);
-    }
-
-    public function activate_key(string $key): bool|array
-    {
-        @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
-
-        $response = wp_remote_post($this->api_url . 'license/activate/' . $this->plugin_slug, [
-            'timeout' => 20,
-            'httpversion' => '1.1',
-            'user-agent' => $this->get_user_agent(),
-            'headers' => [
-                'Content-Type' => 'application/json;charset=' . get_bloginfo('charset'),
-            ],
-            'body' => wp_json_encode([
-                'license_key' => $key,
-                'domain' => get_option('siteurl'),
-            ]),
-        ]);
-
-        return $this->decode_response($response);
-    }
-
-    protected function decode_response($response): bool|array
-    {
-        if (is_wp_error($response)) {
-            return false;
+            return $this->decode_response($response);
         }
 
-        if (wp_remote_retrieve_response_code($response) != '200') {
-            return false;
+        public function get_info(): bool|array
+        {
+            @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
+
+            $response = wp_remote_get($this->api_url . 'products/info/' . $this->plugin_slug, [
+                'timeout' => 20,
+                'httpversion' => '1.1',
+                'user-agent' => $this->get_user_agent(),
+            ]);
+
+            return $this->decode_response($response);
         }
 
-        $data = json_decode(wp_remote_retrieve_body($response), true);
-        if (empty($data)) {
-            return false;
+        public function expire_data(): bool|array
+        {
+            @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
+
+            $response = wp_remote_post($this->api_url . 'license/expire/' . $this->plugin_slug, [
+                'timeout' => 20,
+                'httpversion' => '1.1',
+                'user-agent' => $this->get_user_agent(),
+                'headers' => [
+                    'Content-Type' => 'application/json;charset=' . get_bloginfo('charset'),
+                ],
+                'body' => wp_json_encode([
+                    'domain' => get_option('siteurl'),
+                ]),
+            ]);
+
+            return $this->decode_response($response);
         }
 
-        return $data;
+        public function activate_key(string $key): bool|array
+        {
+            @set_time_limit(30); // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
+
+            $response = wp_remote_post($this->api_url . 'license/activate/' . $this->plugin_slug, [
+                'timeout' => 20,
+                'httpversion' => '1.1',
+                'user-agent' => $this->get_user_agent(),
+                'headers' => [
+                    'Content-Type' => 'application/json;charset=' . get_bloginfo('charset'),
+                ],
+                'body' => wp_json_encode([
+                    'license_key' => $key,
+                    'domain' => get_option('siteurl'),
+                ]),
+            ]);
+
+            return $this->decode_response($response);
+        }
+
+        protected function decode_response($response): bool|array
+        {
+            if (is_wp_error($response)) {
+                return false;
+            }
+
+            if (wp_remote_retrieve_response_code($response) != '200') {
+                return false;
+            }
+
+            $data = json_decode(wp_remote_retrieve_body($response), true);
+            if (empty($data)) {
+                return false;
+            }
+
+            return $data;
+        }
     }
 }
--- includes/update.php
+++ includes/update.php
@@ -16,8 +16,8 @@ final class RY_WTP_Update
             RY_WTP::update_option('version', '3.0.0');
         }
 
-        if (version_compare($now_version, '3.6.0', '<')) {
-            RY_WTP::update_option('version', '3.6.0', true);
+        if (version_compare($now_version, '3.6.1', '<')) {
+            RY_WTP::update_option('version', '3.6.1', true);
         }
     }
 }
