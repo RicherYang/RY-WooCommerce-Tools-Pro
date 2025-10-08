0eab6da7945a560ca10ec1e9cb00bf4d26d35892
Richer Yang <fantasyworldidvtw@gmail.com>
2025-10-08 17:45:48 +0800

    調整連線授權伺服器內容

--- includes/link-server.php
+++ includes/link-server.php
@@ -17,6 +17,15 @@ final class RY_WTP_LinkServer extends RY_Abstract_Link_Server
         return self::$_instance;
     }
 
+    protected function get_base_info(): array
+    {
+        return [
+            'plugin' => RY_WTP_VERSION,
+            'wp' => get_bloginfo('version'),
+            'wc' => WC_VERSION,
+        ];
+    }
+
     protected function get_user_agent()
     {
         return sprintf(
