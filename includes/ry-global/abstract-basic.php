d49527df73af062c5c273c82eea1f66d4684a1ce
Richer Yang <fantasyworldidvtw@gmail.com>
2026-03-01 10:43:13 +0800

    修正可能的 PHP 參數型態限制錯誤

--- includes/ry-global/abstract-basic.php
+++ includes/ry-global/abstract-basic.php
@@ -12,12 +12,12 @@ if (!class_exists('RY_Abstract_Basic', false)) {
             return get_option(static::OPTION_PREFIX . $option, $default);
         }
 
-        public static function update_option(string $option, $value, ?bool $autoload = null): bool
+        public static function update_option(string $option, mixed $value, ?bool $autoload = null)
         {
             return update_option(static::OPTION_PREFIX . $option, $value, $autoload);
         }
 
-        public static function delete_option(string $option): bool
+        public static function delete_option(string $option)
         {
             return delete_option(static::OPTION_PREFIX . $option);
         }
@@ -27,12 +27,12 @@ if (!class_exists('RY_Abstract_Basic', false)) {
             return get_transient(static::OPTION_PREFIX . $transient);
         }
 
-        public static function set_transient(string $transient, $value, int $expiration = 0): bool
+        public static function set_transient(string $transient, mixed $value, int $expiration = 0)
         {
             return set_transient(static::OPTION_PREFIX . $transient, $value, $expiration);
         }
 
-        public static function delete_transient(string $transient): bool
+        public static function delete_transient(string $transient)
         {
             return delete_transient(static::OPTION_PREFIX . $transient);
         }
