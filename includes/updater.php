292c7c71bc4eab2aa5f515296b32fdf5c45202c0
Richer Yang <fantasyworldidvtw@gmail.com>
2026-06-22 16:57:05 +0800

    調整手動檢查更新的邏輯

--- includes/updater.php
+++ includes/updater.php
@@ -19,7 +19,7 @@ final class RY_WTP_Updater
     protected function do_init(): void
     {
         add_filter('update_plugins_ry-plugin.com', [$this, 'update_plugin'], 10, 2);
-        add_filter('pre_set_site_transient_update_plugins', [$this, 'add_basic_plugin']);
+        add_filter('pre_set_site_transient_update_plugins', [$this, 'check_basic_plugin']);
 
         add_filter('plugins_api', [$this, 'modify_plugin_details'], 10, 3);
     }
@@ -33,24 +33,15 @@ final class RY_WTP_Updater
         return RY_WTP_LinkServer::instance()->check_version();
     }
 
-    public function add_basic_plugin($updates)
+    public function check_basic_plugin($updates)
     {
-        if (isset($updates->response, $updates->response[RY_WTP_PLUGIN_BASENAME])) {
-            if (isset($updates->no_update[RY_WT_PLUGIN_BASENAME]) && !isset($updates->response[RY_WT_PLUGIN_BASENAME])) {
-                $raw_response = wp_remote_get('https://api.wordpress.org/plugins/info/1.0/ry-woocommerce-tools.json');
-                if (!is_wp_error($raw_response) && 200 === wp_remote_retrieve_response_code($raw_response)) {
-                    $response = json_decode(wp_remote_retrieve_body($raw_response), true);
-                    if ($response && is_array($response)) {
-                        if (version_compare($response['version'], $updates->no_update[RY_WT_PLUGIN_BASENAME]->new_version, '>')) {
-                            $updates->response[RY_WT_PLUGIN_BASENAME] = $updates->no_update[RY_WT_PLUGIN_BASENAME];
-                            unset($updates->no_update[RY_WT_PLUGIN_BASENAME]);
-                            $updates->response[RY_WT_PLUGIN_BASENAME]->new_version = $response['version'];
-                            $updates->response[RY_WT_PLUGIN_BASENAME]->tested = $response['tested'];
-                            $updates->response[RY_WT_PLUGIN_BASENAME]->requires_php = $response['requires_php'];
-                            $updates->response[RY_WT_PLUGIN_BASENAME]->package = $response['versions'][$response['version']];
-                        }
-                    }
-                }
+        if (defined('RY_WT_VERSION')) {
+            if (isset($updates->response, $updates->response[RY_WTP_PLUGIN_BASENAME])) {
+                return $this->get_official_info($updates);
+            }
+
+            if (version_compare(RY_WT_VERSION, RY_WTP::MIN_TOOLS_VERSION, '<')) {
+                return $this->get_official_info($updates);
             }
         }
 
@@ -73,5 +64,35 @@ final class RY_WTP_Updater
         }
 
         return $result;
+    }
+
+    protected function get_official_info($updates)
+    {
+        if (isset($updates->response[RY_WT_PLUGIN_BASENAME])) {
+            return $updates;
+        }
+
+        if (!isset($updates->no_update[RY_WT_PLUGIN_BASENAME])) {
+            return $updates;
+        }
+
+        $raw_response = wp_remote_get('https://api.wordpress.org/plugins/info/1.0/ry-woocommerce-tools.json');
+        if (!is_wp_error($raw_response) && 200 === wp_remote_retrieve_response_code($raw_response)) {
+            $response = json_decode(wp_remote_retrieve_body($raw_response), true);
+            if ($response && is_array($response)) {
+                if (version_compare($response['version'], RY_WT_VERSION, '>')) {
+                    $updates->response[RY_WT_PLUGIN_BASENAME] = $updates->no_update[RY_WT_PLUGIN_BASENAME];
+                    unset($updates->no_update[RY_WT_PLUGIN_BASENAME]);
+                    $updates->response[RY_WT_PLUGIN_BASENAME]->new_version = $response['version'];
+                    $updates->response[RY_WT_PLUGIN_BASENAME]->tested = $response['tested'];
+                    $updates->response[RY_WT_PLUGIN_BASENAME]->requires = $response['requires'];
+                    $updates->response[RY_WT_PLUGIN_BASENAME]->requires_php = $response['requires_php'];
+                    $updates->response[RY_WT_PLUGIN_BASENAME]->requires_plugins = $response['requires_plugins'];
+                    $updates->response[RY_WT_PLUGIN_BASENAME]->package = $response['versions'][$response['version']];
+                }
+            }
+        }
+
+        return $updates;
     }
 }
